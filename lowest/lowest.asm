50e5c119c3fb71dd5b79d930ed6acba53f506dab
AnanduRamachandran <48447735+AnanduRamachandran@users.noreply.github.com>
2021-06-07 14:50:37 +0530

    Done

---
+++ lowest/lowest.asm
@@ -0,0 +1,17 @@
+mov r0, #9
+	mov dptr, #8200H
+	movc a, @a+dptr
+	mov b, a
+back: inc dptr
+	clr a
+	movc a, @a+dptr
+	cjne a,b, check
+check:  JNC next
+		mov b,a
+next: djnz r0,back
+	   mov r4, b
+		
+org 8200h
+	db 10h,22h,50h,70h,90h,11h,21h,53h,91h,76h
+		
+END
